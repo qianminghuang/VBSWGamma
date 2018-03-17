94d2670dbf86a4e5c7ef1bc02cca2f8d9c9432d5
qianming <huangqianming@pku.edu.cn>
2018-03-17 08:21:54 -0500

    del

--- PKUTreeMaker/test/statusreportmu.sh
+++
@@ -1,9 +0,0 @@
-crab status -d crab_SMu16B-v1
-crab status -d crab_SMu16C-v1
-crab status -d crab_SMu16D-v1
-crab status -d crab_SMu16E-v1
-crab status -d crab_SMu16F-v1
-crab status -d crab_SMu16G-v1
-crab status -d crab_SMu16H-v1
-crab status -d crab_SMu16H-v2
-
