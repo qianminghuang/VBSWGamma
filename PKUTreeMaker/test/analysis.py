c51b8429cbccef9ee1a97df7ffcdeab39e5ce000
Qianming <huangqianming@pku.edu.cn>
2019-04-01 23:35:27 +0200

    L1 prefiring

--- PKUTreeMaker/test/analysis.py
+++ PKUTreeMaker/test/analysis.py
@@ -145,7 +145,7 @@ process.prefiringweight = cms.EDProducer("L1ECALPrefiringWeightProducer",
 #			         L1Maps = cms.string(relBase+"/src/L1Prefiring/EventWeightProducer/files/L1PrefiringMaps_new.root"),
                                 # L1Maps = cms.string("L1PrefiringMaps_new.root"), # update this line with the location of this file
 				L1Maps = cms.string("L1PrefiringMaps_new.root"),
-                                 DataEra = cms.string("2017BtoF"), #Use 2016BtoH for 2016
+                                 DataEra = cms.string("2016BtoH"), #Use 2016BtoH for 2016
                                  UseJetEMPt = cms.bool(False), #can be set to true to use jet prefiring maps parametrized vs pt(em) instead of pt
                                  PrefiringRateSystematicUncty = cms.double(0.2) #Minimum relative prefiring uncty per object
                                  )
--- PKUTreeMaker/test/ntuple-aqgc/analysis.py
+++ PKUTreeMaker/test/ntuple-aqgc/analysis.py
@@ -145,7 +145,7 @@ process.prefiringweight = cms.EDProducer("L1ECALPrefiringWeightProducer",
                                  TheJets = cms.InputTag("slimmedJets"),
 				L1Maps = cms.string("L1PrefiringMaps_new.root"),
                                  #L1Maps = cms.string("L1PrefiringMaps_new.root"), # update this line with the location of this file
-                                 DataEra = cms.string("2017BtoF"), #Use 2016BtoH for 2016
+                                 DataEra = cms.string("2016BtoH"), #Use 2016BtoH for 2016
                                  UseJetEMPt = cms.bool(False), #can be set to true to use jet prefiring maps parametrized vs pt(em) instead of pt
                                  PrefiringRateSystematicUncty = cms.double(0.2) #Minimum relative prefiring uncty per object
                                  )
